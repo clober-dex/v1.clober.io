04aeb088f5f25c70533e0de4c97b5c75fdc9be19
graykode <nlkey2022@gmail.com>
2023-09-21 20:29:17 +0900

    fix: import of text-styles.ts

--- themes/text-styles.ts
+++ themes/text-styles.ts
@@ -1,9 +1,10 @@
 export const textStyles = {
   body2Bold:
-    'text-[16px] leading-[22px] font-sans not-italic -tracking-[.005em] ',
+    'text-[16px] leading-[22px] font-semibold font-sans not-italic -tracking-[0.005em] ',
   body3Bold:
-    'text-[14px] leading-[20px] font-sans not-italic -tracking-[.005em] ',
+    'text-[14px] leading-[20px] font-semibold font-sans not-italic -tracking-[0.005em] ',
   body3_5:
-    'text-[13px] leading-[18px] font-sans not-italic -tracking-[.005em] ',
-  body4: 'text-[12px] leading-[16px] font-sans not-italic -tracking-[.005em] ',
+    'text-[13px] leading-[18px] font-medium font-sans not-italic -tracking-[0.005em] ',
+  body4:
+    'text-[12px] leading-[16px] font-medium font-sans not-italic -tracking-[0.005em] ',
 }
