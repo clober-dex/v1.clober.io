2c46dcc6c875cc1f0c6fd8ee431d559fd57f940b
graykode <nlkey2022@gmail.com>
2023-10-04 13:36:21 +0900

    fix: footer css

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,21 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="sm:flex bg-transparent">
-      <div className="fixed bottom-0 left-0 right-0 flex h-8 justify-between items-center px-4 text-xs text-gray-500">
-        <span>
-          Support:{' '}
-          <a
-            href="mailto:official@clober.io"
-            className="text-gray-500 hover:text-blue-500"
-          >
-            official@clober.io
-          </a>
-        </span>
-        <span>
-          Powered by Clober (v
-          {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 7)})
-        </span>
+    <div className="flex h-8 justify-between items-center px-4 text-xs text-gray-500 bg-transparent mb-1">
+      <div className="flex items-center">
+        Support:{' '}
+        <a
+          href="mailto:official@clober.io"
+          className="text-gray-500 hover:text-blue-500"
+        >
+          official@clober.io
+        </a>
+      </div>
+      <div className="flex w-auto ml-auto">
+        Powered by Clober (v
+        {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 7)})
       </div>
     </div>
   )
