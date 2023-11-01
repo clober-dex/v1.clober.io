37950682d7acff5d7956e548ba95f266219970a7
graykode <nlkey2022@gmail.com>
2023-11-01 17:58:36 +0900

    style: fix open order card style

--- components/card/open-order-card.tsx
+++ components/card/open-order-card.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { OutlinkSvg } from '../svg/outlink-svg'
 import { OpenOrder } from '../../model/open-order'
 import { formatUnits } from '../../utils/bigint'
+import { ActionButton } from '../button/action-button'
 
 export const OpenOrderCard = ({
   openOrder,
@@ -11,10 +12,12 @@ export const OpenOrderCard = ({
   openOrder: OpenOrder
 }) => {
   return (
-    <div className="flex flex-col gap-3 bg-gray-900 rounded p-4" {...props}>
+    <div className="flex flex-col gap-4 bg-gray-900 rounded-2xl p-4" {...props}>
       <div className="flex text-sm text-white justify-between">
         <div className="font-bold flex flex-row items-center gap-1">
-          {openOrder.baseSymbol} / {openOrder.quoteSymbol}
+          {formatUnits(openOrder.amount, openOrder.inputCurrency.decimals)}{' '}
+          {openOrder.inputCurrency.symbol} &#x2192; {1600}{' '}
+          {openOrder.outputCurrency.symbol}
           <a
             target="_blank"
             href="https://etherscan.io/tx/0x84a09de087e610fbba59440ea57f7166b3a7178260c485ab8315dd51ad197475"
@@ -29,40 +32,42 @@ export const OpenOrderCard = ({
           {openOrder.isBid ? 'Buy' : 'Sell'}
         </div>
       </div>
-      <div className="flex flex-col gap-1.5 text-xs sm:text-sm">
-        <div className="flex flex-row align-baseline justify-between">
-          <label className="text-gray-200">Price</label>
-          <p className="text-white">{formatUnits(openOrder.price, 18)}</p>
-        </div>
-        <div className="flex flex-row align-baseline justify-between">
-          <label className="text-gray-200">Filled / Amount</label>
-          <p className="text-white">
-            {formatUnits(openOrder.filledAmount, 18)} /{' '}
-            {formatUnits(openOrder.amount, 18)}
-          </p>
-        </div>
-        <div className="flex flex-row align-baseline justify-between">
-          <label className="text-gray-200">Claimable</label>
-          <p className="text-white">
-            {formatUnits(openOrder.claimableAmount, 18)}
-          </p>
+      <div className="flex flex-col text-xs sm:text-sm">
+        <div className="flex flex-col align-baseline justify-between gap-2">
+          <div className="flex flex-row align-baseline justify-between">
+            <label className="text-gray-200">Price</label>
+            <p className="text-white">{formatUnits(openOrder.price, 18)}</p>
+          </div>
+          <div className="flex flex-row align-baseline justify-between">
+            <label className="text-gray-200">Filled</label>
+            <div className="flex flex-row gap-1">
+              <p className="text-white">{'45%'}</p>
+              <p className="text-gray-400">(1200)</p>
+            </div>
+          </div>
+          <div className="w-full bg-gray-200 rounded-lg dark:bg-gray-700">
+            <div
+              className="flex items-center justify-center h-1.5 bg-blue-500 text-xs font-medium text-gray-100 text-center p-0.5 leading-none rounded-lg"
+              style={{
+                width: '45%',
+              }}
+            />
+          </div>
         </div>
       </div>
       <div className="flex w-full gap-3 h-6">
-        <button
-          className="flex flex-1 items-center justify-center p-0 h-6 text-xs border-solid border-white border rounded bg-transparent text-white"
-          disabled={false}
+        <ActionButton
+          className="flex flex-1 items-center justify-center rounded bg-gray-700 hover:bg-blue-600 text-white text-xs sm:text-sm disabled:bg-gray-800 disabled:text-gray-500 py-2 h-6 sm:h-7"
+          disabled={true}
           onClick={() => console.log('claim', [openOrder])}
-        >
-          Claim
-        </button>
-        <button
-          className="flex flex-1 items-center justify-center p-0 h-6 text-xs border-solid border-white border rounded bg-transparent text-white"
+          text={'Claim'}
+        />
+        <ActionButton
+          className="flex flex-1 items-center justify-center rounded bg-gray-700 hover:bg-gray-500 text-white text-xs sm:text-sm disabled:bg-gray-800 disabled:text-gray-500 py-2 h-6 sm:h-7"
           disabled={false}
           onClick={() => console.log('cancel', [openOrder])}
-        >
-          Cancel
-        </button>
+          text={'Cancel'}
+        />
       </div>
     </div>
   )
