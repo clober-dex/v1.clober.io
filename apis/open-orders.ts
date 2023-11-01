f14924976e99e378c3fa7f6a222ba9e694fe78fb
Xavier <ladig1017@gmail.com>
2023-11-01 18:18:40 +0900

    feat: update open order model

--- apis/open-orders.ts
+++ apis/open-orders.ts
@@ -20,15 +20,33 @@ export async function fetchOpenOrders(
       url: SUBGRAPH_URL[chainId],
     },
   )
-  return openOrders.map((openOrder) => ({
-    baseSymbol: openOrder.market.baseToken.symbol,
-    quoteSymbol: openOrder.market.quoteToken.symbol,
-    isBid: openOrder.isBid,
-    txHash: openOrder.txHash as `0x${string}`,
-    timestamp: formatDate(new Date(Number(openOrder.createdAt) * 1000)),
-    price: BigInt(openOrder.price),
-    filledAmount: BigInt(openOrder.baseFilledAmount),
-    amount: BigInt(openOrder.baseAmount),
-    claimableAmount: BigInt(openOrder.baseClaimedAmount),
-  }))
+  return openOrders.map((openOrder) => {
+    const inputCurrency = openOrder.isBid
+      ? openOrder.market.quoteToken
+      : openOrder.market.baseToken
+    const outputCurrency = openOrder.isBid
+      ? openOrder.market.baseToken
+      : openOrder.market.quoteToken
+    return {
+      inputCurrency: {
+        address: getAddress(inputCurrency.id),
+        name: inputCurrency.name,
+        symbol: inputCurrency.symbol,
+        decimals: Number(inputCurrency.decimals),
+      },
+      outputCurrency: {
+        address: getAddress(outputCurrency.id),
+        name: outputCurrency.name,
+        symbol: outputCurrency.symbol,
+        decimals: Number(outputCurrency.decimals),
+      },
+      isBid: openOrder.isBid,
+      txHash: openOrder.txHash as `0x${string}`,
+      timestamp: formatDate(new Date(Number(openOrder.createdAt) * 1000)),
+      price: BigInt(openOrder.price),
+      filledAmount: BigInt(openOrder.baseFilledAmount),
+      amount: BigInt(openOrder.baseAmount),
+      claimableAmount: BigInt(openOrder.baseClaimedAmount),
+    }
+  })
 }
