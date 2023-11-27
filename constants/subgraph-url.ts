03a69272172959da406ddaf96f1334f37604e06e
Xavier <ladig1017@gmail.com>
2023-11-27 17:31:50 +0900

    feat: update arbitrum subgraph rpc

--- constants/subgraph-url.ts
+++ constants/subgraph-url.ts
@@ -6,7 +6,7 @@ export const SUBGRAPH_URL: {
   [CHAIN_IDS.POLYGON]:
     'https://api.studio.thegraph.com/query/49804/core-v1-polygon-subgraph/v1',
   [CHAIN_IDS.ARBITRUM]:
-    'https://api.studio.thegraph.com/query/49804/core-v1-arbitrum-subgraph/v1',
+    'https://subgraph.satsuma-prod.com/f6a8c4889b7b/clober/core-v1-subgraph/api',
   [CHAIN_IDS.COUPON_FINANCE_CHAIN]:
     'https://dev-subgraph.coupon.finance/subgraphs/name/core-v1-subgraph',
   [CHAIN_IDS.POLYGON_ZKEVM]:
